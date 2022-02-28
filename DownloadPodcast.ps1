2a55f5660b6657bced47dbdf95b59f683b344cdd
habeebtc <caryroys@gmail.com>
2022-02-28 09:27:59 -0600

    Adding V1 file

---
+++ DownloadPodcast.ps1
@@ -0,0 +1,17 @@
+﻿param ([string]$uri, [string]$outputfolder)
+
+if(test-path -Path "$($env:TEMP)\tempfile.xml")
+{
+    remove-item -LiteralPath "$($env:TEMP)\tempfile.xml"
+}
+
+invoke-webrequest -uri $uri -OutFile "$($env:TEMP)\tempfile.xml"
+
+$xmlfile = [xml](get-content "$($env:TEMP)\tempfile.xml")
+
+
+foreach($item in Select-Xml -Xml $xmlfile -XPath "/rss/channel/item")
+{
+    $filepath = "$outputfolder\$($item.node.title).mp3".replace("|","-")
+    invoke-webrequest -uri $item.Node.enclosure.url -outfile $filepath
+}
