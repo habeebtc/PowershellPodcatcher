8c7a7ecc51c96eab3e567a94c5439bddc167cec4
habeebtc <caryroys@gmail.com>
2022-03-13 18:22:22 -0500

    A couple more corner cases fixed

--- DownloadPodcast.ps1
+++ DownloadPodcast.ps1
@@ -9,6 +9,11 @@ function normalizeFilename([string]$filename)
     $filename = $filename.Replace(":","-")
     $filename = $filename.Replace(">","}")
     $filename = $filename.Replace("<","{")
+    $filename = $filename.Replace
+	#This is the pattern for trimming out unwanted Unicode chars on powershell 5.1
+	$filename = $filename.Replace([char]0x00A0," ")
+	$filename = $filename.Replace("?"," ")
+	
     return $filename
 }
 
@@ -18,38 +23,41 @@ if(test-path -Path "$($env:TEMP)\tempfile.xml")
 }
 
 invoke-webrequest -uri $uri -OutFile "$($env:TEMP)\tempfile.xml"
-
-$xmlfile = [xml](get-content "$($env:TEMP)\tempfile.xml")
+$xmlfile = [xml](get-content "$($env:TEMP)\tempfile.xml" -encoding utf8)
 
 try
 {
     foreach($item in Select-Xml -Xml $xmlfile -XPath "/rss/channel/item")
     {
         #get file extension
-        $len = ($item.Node.enclosure.url.split("?")[0]).length
-        $ext = $item.Node.enclosure.url.split("?")[0].substring($len - 3,3)
+		$len = ($item.Node.enclosure.url.split("?")[0]).length
+		$ext = $item.Node.enclosure.url.split("?")[0].substring($len - 3,3)
         if($item.node.title.count -gt 1)
         {
-            $titlenode = $item.node.title[0]
+			write-debug "title count greater than 1"
+			$titlenode = $item.node.title[0]
         }
         else
         {
-             $titlenode = $item.node.title
+			write-debug "title count not greater than 1"
+			$titlenode = $item.node.title
         }
 		
-        if($titlenode.innertext -ne $null)
-        {
-              $filepath = "$outputfolder\$(normalizeFilename($titlenode.innertext)).$ext"
-        }
-        else
-        {
-              $filepath = "$outputfolder\$(normalizeFilename($item.node.title)).$ext"
-        }
-        
+		if($titlenode.innertext -ne $null)
+		{
+			write-debug "Inner text is $($titlenode.innertext)"
+			$filepath = "$outputfolder\$(normalizeFilename($titlenode.innertext)).$ext"
+		}
+		else
+		{
+			write-debug "Node title is $($titlenode)"
+			$filepath = "$outputfolder\$(normalizeFilename($titlenode)).$ext"
+		}
+
         if($false -eq (test-path $filepath -IsValid))  
         {
             write-output "path invalid!"
-            $filepath
+			$filepath
         }
         else
         {
