7551a236640c65d191cb195446b61f4fc3987fe5
habeebtc <caryroys@gmail.com>
2022-03-13 19:01:53 -0500

    Create dummy files for debugging mode

--- DownloadPodcast.ps1
+++ DownloadPodcast.ps1
@@ -9,11 +9,12 @@ function normalizeFilename([string]$filename)
     $filename = $filename.Replace(":","-")
     $filename = $filename.Replace(">","}")
     $filename = $filename.Replace("<","{")
-    #This is the pattern for trimming out unwanted Unicode chars on powershell 5.1
-    $filename = $filename.Replace([char]0x00A0," ")
-    $filename = $filename.Replace("?"," ")
+	#This is the pattern for trimming out unwanted Unicode chars on powershell 5.1
+	$filename = $filename.Replace([char]0x00A0," ")
+	$filename = $filename.Replace("?","")
+	$filename = $filename.Replace("  "," ")
 	
-    return $filename
+    return $filename.Trim()
 }
 
 if(test-path -Path "$($env:TEMP)\tempfile.xml")
@@ -24,23 +25,24 @@ if(test-path -Path "$($env:TEMP)\tempfile.xml")
 invoke-webrequest -uri $uri -OutFile "$($env:TEMP)\tempfile.xml"
 $xmlfile = [xml](get-content "$($env:TEMP)\tempfile.xml" -encoding utf8)
 
-try
+
+foreach($item in Select-Xml -Xml $xmlfile -XPath "/rss/channel/item")
 {
-    foreach($item in Select-Xml -Xml $xmlfile -XPath "/rss/channel/item")
-    {
-        #get file extension
+	try
+	{
+		#get file extension
 		$len = ($item.Node.enclosure.url.split("?")[0]).length
 		$ext = $item.Node.enclosure.url.split("?")[0].substring($len - 3,3)
-        if($item.node.title.count -gt 1)
-        {
+		if($item.node.title.count -gt 1)
+		{
 			write-debug "title count greater than 1"
 			$titlenode = $item.node.title[0]
-        }
-        else
-        {
+		}
+		else
+		{
 			write-debug "title count not greater than 1"
 			$titlenode = $item.node.title
-        }
+		}
 		
 		if($titlenode.innertext -ne $null)
 		{
@@ -53,23 +55,29 @@ try
 			$filepath = "$outputfolder\$(normalizeFilename($titlenode)).$ext"
 		}
 
-        if($false -eq (test-path $filepath -IsValid))  
-        {
-            write-output "path invalid!"
+		if($false -eq (test-path $filepath -IsValid))  
+		{
+			write-output "path invalid!"
 			$filepath
-        }
-        else
-        {
-            #simplest possible download management: don't download if file exists
-            if($false -eq (test-path $filepath))
-            {
-                invoke-webrequest -uri $item.Node.enclosure.url -outfile $filepath
-            }
-        }
-    }
-}
-catch
-{
-    Write-Output $_
-     "filepath: $filepath"
+		}
+		else
+		{
+			#simplest possible download management: don't download if file exists
+			#If your debug preference is set to emit debug messages, don't actually download the stream, just create a dummy file
+			if($false -eq (test-path $filepath))
+			{
+				switch($DebugPreference)
+				{
+					'SilentlyContinue' {invoke-webrequest -uri $item.Node.enclosure.url -outfile $filepath}
+					'Continue' {new-item $filepath}
+				}
+			}
+		}
+	}
+	catch
+	{
+		Write-Output $_
+		 "filepath: $filepath"
+		 continue 
+	}
 }
